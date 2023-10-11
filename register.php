0d9fbdb796c2386f40d82f5e27c78618f9d3deaf
Nick Koole <nickkoole@hotmail.com>
2023-10-11 16:25:02 +0200

    #9 mogelijk gemaakt om een nieuwe gebruiker aan te maken in de users.txt file middels de registerpagina

--- register.php
+++ register.php
@@ -1,25 +1,33 @@
 <?php
-    $name = $errName = $errMail = "";
+    $name = $errName = $errMail = $errPassword = "";
 
     if ($_SERVER["REQUEST_METHOD"] == "POST"){
         
         //Eerst worden ongewenste karakters verwijderd
         $name = testInput($_POST["name"]);
         $email = testInput($_POST["email"]);
-        $emailTwo = testInput($_POST["emailTwo"]);
+        $password = testInput($_POST["password"]);
+        $passwordTwo = testInput($_POST["passwordTwo"]);
         
         //Vervolgens wordt gekeken of correcte input gegeven is
         $errName = checkName($name);
-        $errMail = checkEmailTwo($email, $emailTwo);
         $errMail = checkEmail($email);
+        $errPassword = checkPassword($password, $passwordTwo);
         
-        //Indien sprake is van correcte input wordt doorgegaan naar het mogelijk registreren van het account
-        if ($errName == "" && $errMail == "") {
-            registerAccount();
+        //Indien sprake is van correcte input wordt doorgegaan naar het checken of er sprake is van een nieuw account
+        if ($errName == "" && $errMail == "" && $errPassword == "") {
+            
+            if(checkNewAccount($email)) {
+                
+                //Bij het bestaan van een nieuw uniek account wordt deze aangemaakt
+                registerNewAccount($name, $email, $password);
+            } else {
+                $errMail = "Dit emailadres is al in gebruik";
+            }
         }
     }
     
-    showBody($name, $errName, $errMail);
+    showBody($name, $errName, $errMail, $errPassword);
 
     function checkName($name) {    
     
@@ -51,26 +59,64 @@
         }
     }
     
-    function checkEmailTwo($email, $emailTwo) {
-
-        if (!empty($_POST["emailTwo"])) {
+    
+    function checkPassword($password, $passwordTwo) {
+        
+        if (empty($_POST["password"])){
+            return "Er is geen wachtwoord opgegeven";
+        }
+            
+        if (!empty($_POST["passwordTwo"])) {
                 
-            //Als email niet leeg is wordt gekeken of er sprake is van een tweede emailadres welke gelijk moet zijn aan de eerste
-            if ($email == $emailTwo) {
+            //Als password niet leeg is wordt gekeken of er sprake is van een tweede wachtwoord welke gelijk moet zijn aan de eerste
+            if ($password == $passwordTwo) {
                 return "";
             } else {
-                return "Emailadressen moeten gelijk zijn aan elkaar";
+                return "De wachtwoorden moeten gelijk zijn aan elkaar";
             }
         } else {
-            return "Een tweede emailadres moet ingevuld zijn";
+            return "Het wachtwoord moet ter controle nog een keer ingevuld worden";
         }    
     }
 
-    function registerAccount() {
+    function checkNewAccount($email) {
+        
+        $users = fopen("users.txt", "r") or die("Unable to open file!");
+        
+        $accounts = array();        
+        $i = 0;
+        
+        //Leest eerst de users uit het bestand in een array
+        while(!feof($users)) {
+            $accounts[$i] = fgets($users);
+            $i++;
+        }
+        fclose($users);
+    
+        //Vergelijkt de emailadressen van users vervolgens met het opgegeven emailadres
+        $amountOfAccounts = count($accounts);
+        for($x = 0; $x < $amountOfAccounts; $x++) {
+            $checkEmail[$x] = explode("|", $accounts[$x]);
+        
+            if($checkEmail[$x][0] == $email) {
+                return False;
+            } 
+        }
+        
+        return True;            
     }
+    
+    function registerNewAccount($name, $email, $password) {
+        
+        //Zet de nieuw opgegeven user op de volgende line
+        $users = fopen("users.txt", "a") or die("Unable to open file!");
+        $txt = $email . '|' . $name . '|' . $password . PHP_EOL;
+        fwrite($users, $txt);
+        fclose($users);
+    }
+    
 
-
-    function showBody($name, $errName, $errMail){
+    function showBody($name, $errName, $errMail, $errPassword){
         
         //Formulier met naam, emailadres en emailadrescheck
         echo '<br>
@@ -79,8 +125,10 @@
             <input type="text" id="name" name="name" placeholder="John Doe" value="'; echo $name; echo '"><span>'; echo $errName; echo '</span><br>
             <label for="email">Emailadres:</label>
             <input type="text" id="email" name="email" placeholder="j.doe@example.com" value=""><span>'; echo $errMail; echo '</span><br>
-            <label for="emailTwo">Herhaal uw emailadres:</label>
-            <input type="text" id="emailTwo" name="emailTwo" placeholder="j.doe@example.com" value=""><br>';
+            <label for="password">Wachtwoord:</label>
+            <input type="text" id="password" name="password" value=""><span>'; echo $errPassword; echo '</span><br>
+            <label for="passwordTwo">Herhaal uw wachtwoord:</label>
+            <input type="text" id="passwordTwo" name="passwordTwo" value=""><br>';
             
         //Verborgen variabele om ervoor te zorgen dat de registerpagina gevonden kan worden middels de getRequestedPage functie van index.php
         echo '<input type="hidden" name="page" value="register">';
